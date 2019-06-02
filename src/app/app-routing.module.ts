11a6299c142ff246acec13d37149e7265fe2b203
Waseem Balouch <waseembalouch4@gmail.com>
2019-06-03 01:07:21 +0500

    router changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,167 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
 
 const routes: Routes = [
   {
-    path: '', component: LoginComponent, data: {
+    path: "",
+    component: LoginComponent,
+    data: {
       customLayout: true
     }
   },
-  { path: 'home', component: HomeComponent },
   {
-    path: 'register', component: RegisterComponent, data: {
+    path: "register",
+    component: RegisterComponent,
+    data: {
       customLayout: true
     }
   },
+  {
+    path: "",
+    data: {
+      title: "Get Started"
+    },
 
-
-
-
-  // otherwise redirect to home
-  { path: '**', redirectTo: '' }
+    children: [
+      {
+        path: "home",
+        component: HomeComponent
+      },
+      {
+        path: "accordion",
+        loadChildren: "./accordion/accordion.module#AccordionModule",
+        data: {
+          title: "Accordion"
+        }
+      },
+      {
+        path: "alert",
+        loadChildren: "./alert/alert.module#AlertModule",
+        data: {
+          title: "Alert"
+        }
+      },
+      {
+        path: "layout",
+        data: {
+          title: "Layout"
+        },
+        children: [
+          {
+            path: "configuration",
+            loadChildren:
+              "./layout/configuration/configuration.module#ConfigurationModule",
+            data: {
+              title: "Configuration"
+            }
+          },
+          {
+            path: "custom",
+            loadChildren: "./layout/custom/custom.module#CustomModule",
+            data: {
+              title: "Disable Layout"
+              // disableLayout: true
+            }
+          },
+          {
+            path: "content",
+            loadChildren: "./layout/content/content.module#ContentModule",
+            data: {
+              title: "Content"
+            }
+          },
+          {
+            path: "header",
+            loadChildren: "./layout/header/header.module#HeaderModule",
+            data: {
+              title: "Header"
+            }
+          },
+          {
+            path: "sidebar-left",
+            loadChildren:
+              "./layout/sidebar-left/sidebar-left.module#SidebarLeftModule",
+            data: {
+              title: "Sidebar Left"
+            }
+          },
+          {
+            path: "sidebar-right",
+            loadChildren:
+              "./layout/sidebar-right/sidebar-right.module#SidebarRightModule",
+            data: {
+              title: "Sidebar Right"
+            }
+          }
+        ]
+      },
+      {
+        path: "boxs",
+        data: {
+          title: "Boxs"
+        },
+        children: [
+          {
+            path: "box",
+            loadChildren:
+              "./boxs/box-default/box-default.module#BoxDefaultModule",
+            data: {
+              title: "Box"
+            }
+          },
+          {
+            path: "info-box",
+            loadChildren: "./boxs/box-info/box-info.module#BoxInfoModule",
+            data: {
+              title: "Info Box"
+            }
+          },
+          {
+            path: "small-box",
+            loadChildren: "./boxs/box-small/box-small.module#BoxSmallModule",
+            data: {
+              title: "Small Box"
+            }
+          }
+        ]
+      },
+      {
+        path: "dropdown",
+        loadChildren: "./dropdown/dropdown.module#DropdownModule",
+        data: {
+          title: "Dropdown"
+        }
+      },
+      {
+        path: "tabs",
+        loadChildren: "./tabs/tabs.module#TabsModule",
+        data: {
+          title: "Tabs"
+        }
+      }
+    ]
+  },
+  {
+    path: "form",
+    data: {
+      title: "Form"
+    },
+    children: [
+      {
+        path: "input-text",
+        loadChildren: "./form/input-text/input-text.module#InputTextModule",
+        data: {
+          title: "Input Text"
+        }
+      }
+    ]
+  },  // otherwise redirect to home
+  { path: "**", redirectTo: "" }
 ];
-
 
 // const routes: Routes = [
 //   {
@@ -168,4 +304,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
