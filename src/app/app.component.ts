d2b5a2e6cd00f7b0d184898f5158bed499a4ed42
Waseem Bakhsh <92103500@dawlance.com.pk>
2019-06-02 16:46:54 +0500

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LayoutService } from 'angular-admin-lte';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AdminLTEAngular';
+export class AppComponent implements OnInit {
+  public customLayout: boolean;
+
+  constructor(
+    private layoutService: LayoutService
+  ) {}
+
+  ngOnInit() {
+    this.layoutService.isCustomLayout.subscribe((value: boolean) => {
+      this.customLayout = value;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { adminLteConf } from './config/admin-lte.conf';
+
 import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
+
+import { LayoutModule } from 'angular-admin-lte';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { LoadingPageModule, MaterialBarModule } from 'angular-loading-page';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    LayoutModule.forRoot(adminLteConf),
+    LoadingPageModule, MaterialBarModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
